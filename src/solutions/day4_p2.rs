fe8cc0300c0c79e7814a881750af0a6e339d615d
tokata <skygazer227@gmail.com>
2023-12-06 17:08:24 +0900

    refactor day4 solutions

--- src/solutions/day4_p2.rs
+++ src/solutions/day4_p2.rs
@@ -8,28 +8,21 @@ pub fn solution(lines: Vec<&str>) -> String {
 
         let (winning_numbers, picked_numbers) = numbers.split_once(" | ").unwrap();
 
-        let mut winning_numbers_set = HashSet::new();
-        let mut picked_numbers_set = HashSet::new();
+        let winning_numbers: HashSet<i32> = HashSet::from_iter(
+            winning_numbers
+                .split(" ")
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<i32>().unwrap()),
+        );
 
-        for i in 0..((winning_numbers.len() + 1) / 3) {
-            if let Ok(number) = winning_numbers[i * 3..i * 3 + 2].parse::<i32>() {
-                winning_numbers_set.insert(number);
-            } else {
-                winning_numbers_set
-                    .insert(winning_numbers[i * 3 + 1..i * 3 + 2].parse::<i32>().unwrap());
-            }
-        }
+        let picked_numbers: HashSet<i32> = HashSet::from_iter(
+            picked_numbers
+                .split(" ")
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<i32>().unwrap()),
+        );
 
-        for i in 0..((picked_numbers.len() + 1) / 3) {
-            if let Ok(number) = picked_numbers[i * 3..i * 3 + 2].parse::<i32>() {
-                picked_numbers_set.insert(number);
-            } else {
-                picked_numbers_set
-                    .insert(picked_numbers[i * 3 + 1..i * 3 + 2].parse::<i32>().unwrap());
-            }
-        }
-
-        let intersection = &winning_numbers_set & &picked_numbers_set;
+        let intersection = &winning_numbers & &picked_numbers;
 
         match_counts.push(intersection.len());
     }
